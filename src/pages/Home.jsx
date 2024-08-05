1f37a2982103513850648b9f7fb0a436de0b17ba
TusharAmbawata <111219164+TusharAmbawata@users.noreply.github.com>
2024-08-05 23:33:53 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,6 @@ const Home = () => {
       <Row rowID ='3' title='Trending' fetchUrl={requests.requestTrending}/>
       <Row rowID ='4' title='Top Rated' fetchUrl={requests.requestTopRated}/>
       <Row rowID ='5' title='Harry Potter' fetchUrl={requests.requestHarry}/>
-      <MyList/>
     </>
   )
 }
